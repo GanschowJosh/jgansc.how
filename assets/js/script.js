608b82f465625363f8ce9cf0e12e2e9df918a1a0
Joshua Ganschow <ganschowjosh@gmail.com>
2024-11-03 16:20:23 -0600

    adding dark mode functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,28 @@
-// assets/js/script.js
-
-// Example: Console log on page load
+/**
+ * Toggle Dark Mode
+ */
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Welcome to Joshua Ganschow\'s Portfolio Website!');
-  });
-  
-  // Add any global JavaScript functions or event listeners here
-  
+  const toggleSwitch = document.getElementById('darkModeToggle');
+  if (!toggleSwitch) return; // Exit if no toggle exists (for pages without dark mode)
+
+  const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light-mode';
+
+  document.body.classList.add(currentTheme);
+  if (currentTheme === 'dark-mode') {
+      toggleSwitch.checked = true;
+  } else {
+      toggleSwitch.checked = false;
+  }
+
+  toggleSwitch.addEventListener('change', function() {
+      if (this.checked) {
+          document.body.classList.remove('light-mode');
+          document.body.classList.add('dark-mode');
+          localStorage.setItem('theme', 'dark-mode');
+      } else {
+          document.body.classList.remove('dark-mode');
+          document.body.classList.add('light-mode');
+          localStorage.setItem('theme', 'light-mode');
+      }
+  }, false);
+});

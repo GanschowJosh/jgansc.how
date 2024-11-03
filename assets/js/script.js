b1c2a6fa9379827eaf1496c9e2c50b1c8a998451
Joshua Ganschow <ganschowjosh@gmail.com>
2024-11-03 15:21:21 -0600

    Initial commit with pure HTML, CSS, JS setup

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// assets/js/script.js
+
+// Example: Console log on page load
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Welcome to Joshua Ganschow\'s Portfolio Website!');
+  });
+  
+  // Add any global JavaScript functions or event listeners here
+  
